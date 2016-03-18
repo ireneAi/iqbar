3349234fc06c5aa468fc991c57eb482f7884fc37
ireneAi <lianchen_lin@163.com>
2016-03-18 13:44:08 +0800

    new

--- js/iqbar.js
+++ js/iqbar.js
@@ -1,9 +1,10 @@
 ﻿(function(){
 	// 播放视频
 	var btnPlay = document.getElementById('btnPlay'),
+	    play_video = document.getElementById('play_video'),
 	    videoBox = document.getElementById('videoBox');
 
-	btnPlay.onclick = function(){
+	play_video.onclick = btnPlay.onclick = function(){
 		videoBox.style.display = "block";
 	}
 
@@ -80,26 +81,58 @@
 		}
 	}
 	// 老师切换
-	var prevBtn = document.getElementById('nextBtn'),
+	var listtimer,list_index=1,
+	    prevBtn = document.getElementById('prevBtn'),
 	    nextBtn = document.getElementById('nextBtn'),
-	    teachBox = document.getElementById('teachBox');
+	    teachBox = document.getElementById('teachBox'),
+	    bigBox = document.getElementById('tebox'),
+	    techBoxl =teachBox.getElementsByTagName('img').length;
+
+	teachBox.style.width =  techBoxl * 466 + 'px';
+
 	prevBtn.onclick = function(){
-		startMoveT(teachBox,"-440",'left')
+		console.log(list_index)
+		list_index--		
+		if(list_index <= -1 ){list_index = techBoxl-2 ;}
+		startMoveL(-list_index*466)
+	}
+	nextBtn.onclick = function (){	
+		console.log(list_index)		
+		if(list_index > techBoxl-2){list_index = 0}	
+		startMoveL(-list_index*466)
+		list_index ++
+	}
+	function next_l(){
+		if(list_index > techBoxl-1){list_index = 0}	
+		startMoveL(-list_index*466)
+		list_index ++
+	}
+	bigBox.onmouseover = function(){
+		clearInterval(play_l)
 	}
 
-	function startMoveT(id,iTarget){
-		clearInterval(timer);
-		timer = setInterval(function ()
+	bigBox.onmouseout = function(){
+		play_l = setInterval(next_l,3000)
+	}
+	var play_l = setInterval(next_l,3000)
+
+	function startMoveL(iTarget){
+		clearInterval(listtimer);
+		listtimer = setInterval(function ()
 		{
-			doMoveT(id,iTarget)
-		}, 100)	
+			doMoveL(iTarget)
+		}, 30)	
 	}
-	function doMoveT(id,target,l){
-		var cur=Math.round(parseFloat(getStyle(id,l)));
-		var iSpeed = (target - cur) / 7;
+	function doMoveL(iTarget)
+	{		
+		var iSpeed = (iTarget - teachBox.offsetLeft) / 10;
 		iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);		
-		id.style.l= (cur+iSpeed)+'px';
+		teachBox.offsetLeft == iTarget ? clearInterval(timer) : teachBox.style.left = teachBox.offsetLeft + iSpeed + "px"
 	}
+
+
+
+	
 	// 需要全屏显示的话
 	// var main = document.getElementById('main');		
 	// if(innerWidth>1280){
